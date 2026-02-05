5892c9a835fbc7b654f3fa342600ec72c3df1cc6
fakezindev <zetabr.bruno@gmail.com>
2026-02-05 11:19:28 -0300

    Service layer, component registration

---
+++ src/main/java/com/fakezindev/webservicesspringboot/services/UserService.java
@@ -0,0 +1,29 @@
+package com.fakezindev.webservicesspringboot.services;
+
+import com.fakezindev.webservicesspringboot.entities.User;
+import com.fakezindev.webservicesspringboot.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.get();
+    }
+}
